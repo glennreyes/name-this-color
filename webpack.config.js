6e91950dadd3e889e8b5dc3cfe7b8e47df4b2bd2
Glenn Reyes <glenn.reyes@styria.com>
2016-04-15 23:00:32 +0200

    To pure development config

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,41 @@ const path = require('path');
 const cssnano = require('cssnano');
 const webpack = require('webpack');
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./webpack.config.prod');
-} else {
-  module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    entry: [path.join(__dirname, 'src'), 'webpack-hot-middleware/client'],
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js',
-      publicPath: '/static/',
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'babel',
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.scss$/,
-          loaders: [
-            'style',
-            ['css?modules', 'localIdentName=[local]_[hash:base64:5]', 'sourceMap'].join('&'),
-            'postcss',
-            'sass?sourceMap',
-          ],
-        },
-      ],
-    },
-    postcss: function() {
-      return [cssnano()]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        },
-      }),
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [path.join(__dirname, 'src'), 'webpack-hot-middleware/client'],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'app.js',
+    publicPath: '/static/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          ['css?modules', 'localIdentName=[local]_[hash:base64:5]', 'sourceMap'].join('&'),
+          'postcss',
+          'sass?sourceMap',
+        ],
+      },
     ],
-  };
-}
+  },
+  postcss: function() {
+    return [cssnano()]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+};
